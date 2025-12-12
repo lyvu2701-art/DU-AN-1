096420262b610f01135061b9aa6d906891736052
tphan <pthi7287@gmail.com>
2025-12-12 10:42:31 +0700

    hehe

--- tang4.js
+++ tang4.js
@@ -8,7 +8,7 @@ if (!tenfolder || !users || !users[DaDangNhap] || !users[DaDangNhap].drive.child
 } else {
     var root = users[DaDangNhap].drive.children[tenfolder];
 }
-// gán sự kiện onclick cho thẻ có class 'con3' để mở popup tạo file    
+// gán sự kiện onclick cho thẻ có class 'nuttaofile' để mở popup tạo file    
 document.getElementsByClassName("nuttaofile")[0].onclick = function(){
     document.querySelector(".taofile").style.display="flex";
 }
